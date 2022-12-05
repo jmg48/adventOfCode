a72e2aeb56e5c163e2e800e3d2cc789b1f972849
Jon Glass <jon.glass@eso.com>
2022-12-05 12:15:07 +0000

    Day 3

--- jmg/AdventOfCode.cs
+++ jmg/AdventOfCode.cs
@@ -26,6 +26,14 @@
         [Test]
         public void DayOne()
         {
+            Console.WriteLine(File.ReadAllLines("C:\\git\\input.txt")
+                .Aggregate(
+                    (0, 0),
+                    (a, s) => string.IsNullOrWhiteSpace(s)
+                        ? (Math.Max(a.Item1, a.Item2), 0)
+                        : (a.Item1, a.Item2 + int.Parse(s)),
+                    a => a.Item1));
+
             var totals = new List<int>();
 
             var currentRunningTotal = 0;
@@ -168,6 +176,43 @@
                     Result.Draw => 3,
                     Result.Win => 6,
                 };
+            }
+
+            Console.WriteLine(total);
+        }
+
+        [Test]
+        public void DayThree_1()
+        {
+            var total = 0;
+            using var reader = new StreamReader("C:\\git\\input3.txt");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var bag1 = new HashSet<char>(line.Take(line.Length / 2));
+                var c = line.Skip(line.Length / 2).Where(c => bag1.Contains(c)).Distinct().Single();
+                var i = (int)c;
+                var score = i <= 'Z' ? (i - 'A' + 27) : (i - 'a' + 1);
+                total += score;
+            }
+
+            Console.WriteLine(total);
+        }
+
+        [Test]
+        public void DayThree_2()
+        {
+            var total = 0;
+            using var reader = new StreamReader("C:\\git\\input3.txt");
+            while (!reader.EndOfStream)
+            {
+                var line1 = new HashSet<char>(reader.ReadLine());
+                line1.IntersectWith(reader.ReadLine());
+                line1.IntersectWith(reader.ReadLine());
+                var c = line1.Single();
+                var i = (int)c;
+                var score = i <= 'Z' ? (i - 'A' + 27) : (i - 'a' + 1);
+                total += score;
             }
 
             Console.WriteLine(total);
