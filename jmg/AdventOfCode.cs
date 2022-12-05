ce850274c78ebc2c691e9d11f10590bf26d8e6e9
Jon Glass <jon.glass@eso.com>
2022-12-05 13:04:54 +0000

    Day 4

--- jmg/AdventOfCode.cs
+++ jmg/AdventOfCode.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using NUnit.Framework;
 
     [TestFixture]
@@ -216,6 +217,34 @@
             }
 
             Console.WriteLine(total);
+        }
+
+        [Test]
+        public void DayFour()
+        {
+            var contains = 0;
+            var overlaps = 0;
+            using var reader = new StreamReader("C:\\git\\input4.txt");
+            while (!reader.EndOfStream)
+            {
+                var match = Regex.Match(reader.ReadLine(), @"(\d+)-(\d+),(\d+)-(\d+)");
+                var a = int.Parse(match.Groups[1].Value);
+                var b = int.Parse(match.Groups[2].Value);
+                var c = int.Parse(match.Groups[3].Value);
+                var d = int.Parse(match.Groups[4].Value);
+                if ((a >= c && b <= d) || (c >= a && d <= b))
+                {
+                    contains++;
+                }
+
+                if (b >= c && d >= a)
+                {
+                    overlaps++;
+                }
+            }
+
+            Console.WriteLine(contains);
+            Console.WriteLine(overlaps);
         }
     }
 }
