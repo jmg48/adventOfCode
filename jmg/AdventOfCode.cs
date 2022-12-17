95b8ddbf39603243a5e0a6412fd7e43b73f4101f
Jon Glass <jon.glass@eso.com>
2022-12-17 20:21:26 +0000

    Day 17

--- jmg/AdventOfCode.cs
+++ jmg/AdventOfCode.cs
@@ -3,13 +3,13 @@
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Text.RegularExpressions;
     using Dijkstra.NET.Graph.Simple;
     using Dijkstra.NET.ShortestPath;
     using FluentAssertions;
-    using Newtonsoft.Json.Linq;
     using NUnit.Framework;
 
     [TestFixture]
@@ -955,6 +955,9 @@
         [TestCase(2)]
         public void Day16(int part)
         {
+            var timer = new Stopwatch();
+            timer.Start();
+
             var graph = new Graph();
             var input = File.ReadLines("C:\\git\\input16.txt")
                 .Select(line =>
@@ -994,9 +997,8 @@
             Search(start, twelve, (part == 1 ? 30 : 26, 0), part == 1);
             void Search(uint from, uint[] too, (int T, int Score) previous, bool elephant)
             {
-                for (var i = 0; i < too.Length; i++)
+                foreach (var to in too)
                 {
-                    var to = too[i];
                     var move = Move(from, to, previous);
                     if (move.T >= 0)
                     {
@@ -1017,7 +1019,127 @@
                 }
             }
 
-            Console.WriteLine(highScore);
+            Console.WriteLine($"{highScore} (in {timer.ElapsedMilliseconds}ms)");
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Day17(int part)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var blocks = part switch { 1 => 2022, 2 => 1000000000000 };
+
+            var jets = File.ReadLines("C:\\git\\input17.txt").Single().ToCharArray();
+
+            var sprites = new List<List<Coord<long>>>
+            {
+                new() { new(0, 0), new(1, 0), new(2, 0), new(3, 0) },
+                new() { new(1, 0), new(0, 1), new(1, 1), new(2, 1), new(1, 2) },
+                new() { new(0, 0), new(1, 0), new(2, 0), new(2, 1), new(2, 2) },
+                new() { new(0, 0), new(0, 1), new(0, 2), new(0, 3) },
+                new() { new(0, 0), new(0, 1), new(1, 0), new(1, 1) },
+            };
+
+            var spriteIndex = -1;
+            var jetIndex = -1;
+
+            List<Coord<long>> NextSprite()
+            {
+                spriteIndex = (spriteIndex + 1) % sprites.Count;
+                return sprites[spriteIndex];
+            }
+
+            int NextJet()
+            {
+                jetIndex = (jetIndex + 1) % jets.Length;
+                return jets[jetIndex] == '<' ? -1 : 1;
+            }
+
+            var tower = new HashSet<Coord<long>>(Enumerable.Range(0, 7).Select(i => new Coord<long>(i, 0)));
+
+            (bool Success, List<Coord<long>> To) TryMove(List<Coord<long>> from, long x, long y)
+            {
+                var to = from.Select(c => new Coord<long>(c.X + x, c.Y + y)).ToList();
+                if (to.Any(c => c.X < 0 || c.X > 6 || tower.Contains(c)))
+                {
+                    return (false, from);
+                }
+
+                return (true, to);
+            }
+
+            var sigs = new Dictionary<(long X, int SpriteIndex, int JetIndex), (long Blocks, long Height)>();
+
+            long cycleLength = 0;
+            long cycleHeight = 0;
+            long cycleStability = 0;
+            var cycleFound = false;
+
+            long height = 0;
+            for (long i = 0; i < blocks; i++)
+            {
+                var (_, next) = TryMove(NextSprite(), 2, height + 4);
+
+                var sprite = next;
+                while (true)
+                {
+                    var (_, jetted) = TryMove(sprite, NextJet(), 0);
+
+                    var (success, fallen) = TryMove(jetted, 0, -1);
+                    if (success)
+                    {
+                        sprite = fallen;
+                    }
+                    else
+                    {
+                        foreach (var coord in fallen)
+                        {
+                            tower.Add(coord);
+                            height = Math.Max(height, coord.Y);
+                        }
+
+                        if (cycleFound)
+                        {
+                            if ((blocks - i - 1) % cycleLength == 0)
+                            {
+                                var cycles = (blocks - i - 1) / cycleLength;
+                                Console.WriteLine($"Found by cycle detection: {height + (cycles * cycleHeight)} in {timer.ElapsedMilliseconds}ms");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            var sig = (fallen.Min(c => c.X), spriteIndex, jetIndex);
+                            if (sigs.TryGetValue(sig, out var prevSig))
+                            {
+                                if (cycleLength == i - prevSig.Blocks && cycleHeight == height - prevSig.Height)
+                                {
+                                    cycleStability++;
+                                }
+                                else
+                                {
+                                    cycleLength = i - prevSig.Blocks;
+                                    cycleHeight = height - prevSig.Height;
+                                    cycleStability = 0;
+                                }
+
+                                if (cycleStability > cycleLength)
+                                {
+                                    cycleFound = true;
+                                }
+                            }
+
+                            sigs[sig] = (i, height);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Found by enumeration: {height} in {timer.ElapsedMilliseconds}ms");
         }
 
         private Coord Follow(Coord head, Coord tail)
@@ -1054,6 +1176,8 @@
         }
 
         private record Coord(int X, int Y);
+
+        private record Coord<T>(T X, T Y);
 
         private class Monkey
         {
