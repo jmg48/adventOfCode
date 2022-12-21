3a112b6b9d828dbe810777a7c657ae6032321007
Jon Glass <jon.glass@eso.com>
2022-12-21 08:10:48 +0000

    Day 21

--- jmg/AdventOfCode.cs
+++ jmg/AdventOfCode.cs
@@ -6,9 +6,7 @@
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
-    using System.Net.Http;
     using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
     using Dijkstra.NET.Graph.Simple;
     using Dijkstra.NET.ShortestPath;
     using FluentAssertions;
@@ -1379,6 +1377,150 @@
             Console.WriteLine(result);
         }
 
+        [Test]
+        public void Day21_1()
+        {
+            var monkeys = new Dictionary<string, Lazy<long>>();
+            foreach (var match in File.ReadLines("C:\\git\\input21.txt").Select(line => Regex.Match(line, @"(\w+): ((\w+) ([+\-*/]) (\w+)|\d+)")))
+            {
+                monkeys[match.Groups[1].Value] = new Lazy<long>(() =>
+                {
+                    if (long.TryParse(match.Groups[2].Value, out var value))
+                    {
+                        return value;
+                    }
+
+                    checked
+                    {
+                        var arg1 = monkeys[match.Groups[3].Value].Value;
+                        var arg2 = monkeys[match.Groups[5].Value].Value;
+                        return match.Groups[4].Value switch { "+" => arg1 + arg2, "-" => arg1 - arg2, "*" => arg1 * arg2, "/" => arg1 / arg2 };
+                    }
+                });
+            }
+
+            Console.WriteLine(monkeys["root"].Value);
+        }
+
+        [Test]
+        public void Day21_2()
+        {
+            var monkeys = new Dictionary<string, Lazy<MonkeyDo>>();
+            var rootArg1 = string.Empty;
+            var rootArg2 = string.Empty;
+            foreach (var match in File.ReadLines("C:\\git\\input21.txt").Select(line => Regex.Match(line, @"(\w+): ((\w+) ([+\-*/]) (\w+)|\d+)")))
+            {
+                var name = match.Groups[1].Value;
+                var expr = match.Groups[2].Value;
+                var arg1 = match.Groups[3].Value;
+                var op = match.Groups[4].Value;
+                var arg2 = match.Groups[5].Value;
+
+                if (name == "root")
+                {
+                    rootArg1 = arg1;
+                    rootArg2 = arg2;
+                }
+
+                monkeys[name] = new Lazy<MonkeyDo>(() =>
+                {
+                    if (name == "humn")
+                    {
+                        return new MonkeyDo.Unknown("humn");
+                    }
+
+                    if (long.TryParse(expr, out var value))
+                    {
+                        return new MonkeyDo.Constant(value);
+                    }
+
+                    return new MonkeyDo.Arithmetic(monkeys[arg1].Value, op, monkeys[arg2].Value);
+                });
+            }
+
+            MonkeyDo Reduce(MonkeyDo arg)
+            {
+                switch (arg)
+                {
+                    case MonkeyDo.Unknown unknown:
+                        return unknown;
+                    case MonkeyDo.Constant constant:
+                        return constant;
+                    case MonkeyDo.Arithmetic arithmetic:
+                        var arg1 = Reduce(arithmetic.Arg1);
+                        var arg2 = Reduce(arithmetic.Arg2);
+                        if (arg1 is MonkeyDo.Constant val1 && arg2 is MonkeyDo.Constant val2)
+                        {
+                            return new MonkeyDo.Constant(arithmetic.Op switch
+                            {
+                                "+" => val1.Value + val2.Value,
+                                "-" => val1.Value - val2.Value,
+                                "*" => val1.Value * val2.Value,
+                                "/" => val1.Value / val2.Value,
+                            });
+                        }
+
+                        return new MonkeyDo.Arithmetic(arg1, arithmetic.Op, arg2);
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(arg));
+                }
+            }
+
+            var lhs = Reduce(monkeys[rootArg1].Value);
+            var rhs = Reduce(monkeys[rootArg2].Value);
+
+            while (lhs is MonkeyDo.Arithmetic expression && rhs is MonkeyDo.Constant constant)
+            {
+                checked
+                {
+                    Console.WriteLine(lhs);
+                    Console.WriteLine(rhs);
+                    Console.WriteLine();
+
+                    if (expression.Arg1 is MonkeyDo.Constant arg1)
+                    {
+                        lhs = expression.Arg2;
+                        switch (expression.Op)
+                        {
+                            case "+":
+                                rhs = new MonkeyDo.Constant(constant.Value - arg1.Value);
+                                break;
+                            case "-":
+                                rhs = new MonkeyDo.Constant(arg1.Value - constant.Value);
+                                break;
+                            case "*":
+                                rhs = new MonkeyDo.Constant(constant.Value / arg1.Value);
+                                break;
+                            case "/":
+                                rhs = new MonkeyDo.Constant(arg1.Value / constant.Value);
+                                break;
+                        }
+                    }
+                    else if (expression.Arg2 is MonkeyDo.Constant arg2)
+                    {
+                        lhs = expression.Arg1;
+                        switch (expression.Op)
+                        {
+                            case "+":
+                                rhs = new MonkeyDo.Constant(constant.Value - arg2.Value);
+                                break;
+                            case "-":
+                                rhs = new MonkeyDo.Constant(constant.Value + arg2.Value);
+                                break;
+                            case "*":
+                                rhs = new MonkeyDo.Constant(constant.Value / arg2.Value);
+                                break;
+                            case "/":
+                                rhs = new MonkeyDo.Constant(constant.Value * arg2.Value);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{rhs} = {lhs}");
+        }
+
         private Coord Follow(Coord head, Coord tail)
         {
             var horiz = head.X - tail.X;
@@ -1425,6 +1567,24 @@
             public static Resource operator -(Resource a, Resource b) => new(a.Ore - b.Ore, a.Clay - b.Clay, a.Obsidian - b.Obsidian, a.Geode - b.Geode);
 
             public bool IsOverdrawn() => this.Ore < 0 || this.Clay < 0 || this.Obsidian < 0 || this.Geode < 0;
+        }
+
+        private record MonkeyDo
+        {
+            public record Unknown(string Symbol) : MonkeyDo
+            {
+                public override string ToString() => this.Symbol;
+            }
+
+            public record Constant(long Value) : MonkeyDo
+            {
+                public override string ToString() => $"{this.Value}";
+            }
+
+            public record Arithmetic(MonkeyDo Arg1, string Op, MonkeyDo Arg2) : MonkeyDo
+            {
+                public override string ToString() => $"({this.Arg1} {this.Op} {this.Arg2})";
+            }
         }
 
         [DebuggerDisplay("{Prev.Value,5} >> {Value,5} >> {Next.Value,5}")]
