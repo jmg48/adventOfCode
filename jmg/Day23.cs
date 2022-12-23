a776d6954aeb8a66797603f391faa4d9e15a4fb0
Jon Glass <jon.glass@eso.com>
2022-12-23 06:26:51 +0000

    Tidy up

--- jmg/Day23.cs
+++ jmg/Day23.cs
@@ -14,58 +14,32 @@
         {
             var grid = new HashSet<Coord>();
             var input = File.ReadAllLines("C:\\git\\input23.txt").Select(line => line.ToList()).ToList();
+            for (var i = 0; i < input.Count; i++)
             {
-                for (var i = 0; i < input.Count; i++)
+                for (var j = 0; j < input[i].Count; j++)
                 {
-                    for (var j = 0; j < input[i].Count; j++)
+                    if (input[i][j] == '#')
                     {
-                        if (input[i][j] == '#')
-                        {
-                            grid.Add(new Coord(j, i));
-                        }
+                        grid.Add(new Coord(j, i));
                     }
                 }
             }
 
-            var movements = new[]
-            {
-                new Coord(0, -1),
-                new Coord(0, 1),
-                new Coord(-1, 0),
-                new Coord(1, 0),
-            };
-
-            var diagonals = new[]
-            {
-                new Coord(-1, -1),
-                new Coord(1, -1),
-                new Coord(-1, 1),
-                new Coord(1, 1),
-                new Coord(-1, -1),
-                new Coord(-1, 1),
-                new Coord(1, -1),
-                new Coord(1, 1),
-            };
+            var movements = new[] { new Coord(0, -1), new Coord(0, 1), new Coord(-1, 0), new Coord(1, 0) };
+            var diagonals = new[] { new Coord(-1, -1), new Coord(1, -1), new Coord(-1, 1), new Coord(1, 1), new Coord(-1, -1), new Coord(-1, 1), new Coord(1, -1), new Coord(1, 1) };
+            var allDirections = movements.Concat(diagonals).Distinct().ToList();
 
             var round = 1;
             while (true)
             {
                 var possibleMoves = new List<(Coord From, Coord To)>();
-                foreach (var elf in grid)
+                foreach (var elf in grid.Where(elf => allDirections.Any(move => grid.Contains(elf + move))))
                 {
-                    if (movements.Concat(diagonals).All(move => !grid.Contains(elf + move)))
-                    {
-                        continue;
-                    }
-
                     for (var i = 0; i < 4; i++)
                     {
-                        var movement = movements[i];
-                        var diag1 = diagonals[i * 2];
-                        var diag2 = diagonals[(i * 2) + 1];
-                        if (!grid.Contains(elf + movement) && !grid.Contains(elf + diag1) && !grid.Contains(elf + diag2))
+                        if (!grid.Contains(elf + movements[i]) && !grid.Contains(elf + diagonals[i * 2]) && !grid.Contains(elf + diagonals[(i * 2) + 1]))
                         {
-                            possibleMoves.Add((elf, elf + movement));
+                            possibleMoves.Add((elf, elf + movements[i]));
                             break;
                         }
                     }
@@ -79,17 +53,9 @@
                     moved = true;
                 }
 
-                movements = movements.Skip(1).Concat(movements.Take(1)).ToArray();
-                diagonals = diagonals.Skip(2).Concat(diagonals.Take(2)).ToArray();
-
                 if (round == 10)
                 {
-                    var left = grid.Min(elf => elf.X);
-                    var right = grid.Max(elf => elf.X);
-                    var top = grid.Max(elf => elf.Y);
-                    var bottom = grid.Min(elf => elf.Y);
-                    var result = ((right - left + 1) * (top - bottom + 1)) - grid.Count;
-                    Console.WriteLine($"Part 1: {result}");
+                    Console.WriteLine($"Part 1: {((grid.Max(elf => elf.X) - grid.Min(elf => elf.X) + 1) * (grid.Max(elf => elf.Y) - grid.Min(elf => elf.Y) + 1)) - grid.Count}");
                 }
 
                 if (!moved)
@@ -98,6 +64,8 @@
                     return;
                 }
 
+                movements = movements.Skip(1).Concat(movements.Take(1)).ToArray();
+                diagonals = diagonals.Skip(2).Concat(diagonals.Take(2)).ToArray();
                 round++;
             }
         }
