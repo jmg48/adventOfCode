8ead61a603466d07ee97bbcaeca81fdde4368342
Jon Glass <jon.glass@eso.com>
2022-12-25 05:47:52 +0000

    Extract digit-to-value conversion

--- Day25.cs
+++ Day25.cs
@@ -12,31 +12,16 @@ internal class Day25
     [Test]
     public void Test()
     {
-        var total = File.ReadAllLines("C:\\git\\advent-of-code\\input25.txt")
-            .Sum(line => line.Select(c => c switch
-                {
-                    '=' => -2,
-                    '-' => -1,
-                    '0' => 0,
-                    '1' => 1,
-                    '2' => 2,
-                })
-                .Aggregate(0L, (value, digit) => (value * 5) + digit));
+        long Val(char c) => c switch { '=' => -2L, '-' => -1L, '0' => 0L, '1' => 1L, '2' => 2L };
+
+        var total = File.ReadLines("C:\\git\\advent-of-code\\input25.txt")
+            .Sum(line => line.Select(Val).Aggregate(0L, (value, digit) => (value * 5) + digit));
 
         var result = new Stack<char>();
         while (total != 0)
         {
-            var (digit, value) = (((total % 5) + 5) % 5) switch
-            {
-                0 => ('0', 0),
-                1 => ('1', 1),
-                2 => ('2', 2),
-                3 => ('=', -2),
-                4 => ('-', -1),
-            };
-
-            result.Push(digit);
-            total = (total - value) / 5;
+            result.Push((((total % 5) + 5) % 5) switch { 0 => '0', 1 => '1', 2 => '2', 3 => '=', 4 => '-' });
+            total = (total - Val(result.Peek())) / 5;
         }
 
         Console.WriteLine(new string(result.ToArray()));
