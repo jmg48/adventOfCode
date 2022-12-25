0fc7e8883b4bb1557b886a7d8fce41df9649b2a2
Jon Glass <jon.glass@eso.com>
2022-12-25 05:39:45 +0000

    Tidy up and (too late) enable checked arithmetic by default

--- Day25.cs
+++ Day25.cs
@@ -1,6 +1,7 @@
 ﻿namespace AdventOfCode;
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -12,40 +13,32 @@ internal class Day25
     public void Test()
     {
         var total = File.ReadAllLines("C:\\git\\advent-of-code\\input25.txt")
-            .Sum(line => line.Select(c => c switch { '=' => -2, '-' => -1, '0' => 0, '1' => 1, '2' => 2 })
-                .Aggregate(default(long), (value1, digit) => (value1 * 5) + digit));
+            .Sum(line => line.Select(c => c switch
+                {
+                    '=' => -2,
+                    '-' => -1,
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                })
+                .Aggregate(0L, (value, digit) => (value * 5) + digit));
 
-        Console.WriteLine(total);
-
-        var part1 = string.Empty;
+        var result = new Stack<char>();
         while (total != 0)
         {
-            switch (((total % 5) + 5) % 5)
+            var (digit, value) = (((total % 5) + 5) % 5) switch
             {
-                case 0:
-                    part1 += "0";
-                    break;
-                case 1:
-                    part1 += "1";
-                    total -= 1;
-                    break;
-                case 2:
-                    part1 += "2";
-                    total -= 2;
-                    break;
-                case 3:
-                    part1 += "=";
-                    total += 2;
-                    break;
-                case 4:
-                    part1 += "-";
-                    total += 1;
-                    break;
-            }
+                0 => ('0', 0),
+                1 => ('1', 1),
+                2 => ('2', 2),
+                3 => ('=', -2),
+                4 => ('-', -1),
+            };
 
-            total /= 5;
+            result.Push(digit);
+            total = (total - value) / 5;
         }
 
-        Console.WriteLine(new string(part1.Reverse().ToArray()));
+        Console.WriteLine(new string(result.ToArray()));
     }
 }
